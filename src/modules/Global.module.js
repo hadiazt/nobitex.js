479d7f909ef806f99a70f059039ddec5989a02e2
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-11 19:12:58 +0430

    Create Global.module.js

---
+++ src/modules/Global.module.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+module.exports = () => {
+    const config = {
+        method: 'post',
+        url: 'https://api.nobitex.ir/market/global-stats',
+    };
+    return new Promise((resolve) => {
+        axios(config).then(data => {
+            const req = data.data.markets.binance;
+            resolve(req)
+        }).catch(e => { console.log(e.toJSON().message + '\nMake sure entered a correct type') });
+    });
+}
+
