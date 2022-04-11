f8a5b62ba799e8a740567187a08269111c9538b6
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-11 18:27:46 +0430

    Update Price.module.js

--- src/modules/Price.module.js
+++ src/modules/Price.module.js
@@ -1,22 +1,22 @@
 const axios = require('axios');
 
-module.exports = () => {
-
+module.exports = ({ type }) => {
     const config = {
         method: 'get',
-        url: 'https://api.nobitex.ir/v2/orderbook/BTCIRT',
+        url: 'https://api.nobitex.ir/v2/orderbook/' + type,
     };
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         axios(config).then(data => {
             const req = data.data;
-            const time = new Date(req.lastUpdate).toLocaleString()
+            const lastUpdate = new Date(req.lastUpdate).toLocaleString()
+            const lastTradePrice = req.lastTradePrice
+            const buy = req.asks
+            const sell = req.bids
 
-            resolve({
-                lastUpdate: time
-            })
+            resolve({ lastUpdate, lastTradePrice, buy, sell, })
 
-        }).catch(e => { console.log(e?.response?.data?.message || e) });
+        }).catch(e => { console.log(e.toJSON().message + '\nMake sure entered a correct type') });
     });
 
 }
