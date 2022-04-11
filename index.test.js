a6d98fd18d975c11d019263d544b587b6d6b238b
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-11 19:27:53 +0430

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,8 +1,7 @@
 const nobitex = require('./src/main')
 
 // nobitex.price({type:'BTCIRT'}).then(res => console.log(res))
-
-nobitex.trades({ type: 'BTCIRT' }).then(res => console.log(res))
+// nobitex.trades({ type: 'BTCIRT' }).then(res => console.log(res))
 
 // BTCIRT, ETHIRT, LTCIRT, XRPIRT, BCHIRT, BNBIRT, EOSIRT
 // XLMIRT, ETCIRT, TRXIRT, DOGEIRT, UNIIRT, DAIIRT, LINKIRT
@@ -10,3 +9,17 @@ nobitex.trades({ type: 'BTCIRT' }).then(res => console.log(res))
 // LTCUSDT, XRPUSDT, BCHUSDT, BNBUSDT, EOSUSDT, XLMUSDT, ETCUSDT 
 // TRXUSDT, PMNUSDT, DOGEUSDT, UNIUSDT, DAIUSDT, LINKUSDT, DOTUSDT
 // AAVEUSDT, ADAUSDT, SHIBUSDT
+
+
+// nobitex.market({from:'btc',to:'rls'}).then(res => console.log(res))
+/* From Options : */
+// rls, btc, eth, ltc, usdt, xrp, bch, bnb, 
+// eos, xlm, etc, trx, pmn, doge, uni, dai, 
+// link, dot, aave, ada, shib
+
+/* To Options : */
+// usdt , rls
+
+
+// nobitex.global().then(res => console.log(res))
+
