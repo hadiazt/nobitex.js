00980b757a2bda2bf39d7bf453a6964a3b7b3733
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-11 18:45:40 +0430

    finished trades()

---
+++ src/modules/Trades.module.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+module.exports = ({ type }) => {
+    const config = {
+        method: 'get',
+        url: 'https://api.nobitex.ir/v2/trades/' + type,
+    };
+    return new Promise((resolve) => {
+        axios(config).then(data => {
+            const { trades } = data.data;
+            const res = []
+            trades.forEach(trade => {
+                const time = new Date(trade.time).toLocaleString()
+                const price = trade.price
+                const volume = trade.volume
+                const type = trade.type
+                const x = {
+                    time,
+                    price,
+                    volume,
+                    type
+                }
+                res.push(x)
+            })
+            resolve(res)
+        }).catch(e => { console.log(e.toJSON().message + '\nMake sure entered a correct type') });
+    });
+}
