d41289bb304b1b37cbdd11b1fb581a9b9e1fd0fa
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-11 19:13:01 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 exports.price = require('./modules/Price.module')
 exports.trades = require('./modules/Trades.module')
 exports.market = require('./modules/Market.module')
+exports.global = require('./modules/Global.module')
