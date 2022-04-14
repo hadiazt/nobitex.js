eaae48044da25058bc9f0145f3c1fff422aa67ea
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-15 01:48:47 +0430

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -3,13 +3,7 @@ const nobitex = require('./src/main')
 const { token } = require('./file.json')
 
 
-// nobitex.price({type:'BTCIRT'}).then(res => console.log(res))
-// nobitex.trades({ type: 'BTCIRT' }).then(res => console.log(res))
-
-/* Visit  https://github.com/hadiazt/nobitex.js/blob/main/Data/Objects.md#--price--trade-functions-- From Seeing Objects*/
-
-
-// nobitex.market({from:'btc',to:'rls'}).then(res => console.log(res))
+nobitex.market({from:'btc',to:'rls'}).then(res => console.log(res))
 /* From Options : */
 // rls, btc, eth, ltc, usdt, xrp, bch, bnb, 
 // eos, xlm, etc, trx, pmn, doge, uni, dai, 
@@ -18,6 +12,6 @@ const { token } = require('./file.json')
 /* To Options : */
 // usdt , rls
 
-// nobitex.global().then(res => console.log(res))
+nobitex.global().then(res => console.log(res))
 
 nobitex.account({ token: token }).then(res => console.log(res))
