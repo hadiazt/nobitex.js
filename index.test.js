796cf18f75fc7b9a6823c74fc3bc48e166a8e029
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-15 01:30:46 +0430

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const nobitex = require('./src/main')
+const nobitex = require('./src/main') 
 // const nobitex = require(nobitex.js)
 const { token } = require('./file.json')
 
@@ -6,12 +6,9 @@ const { token } = require('./file.json')
 // nobitex.price({type:'BTCIRT'}).then(res => console.log(res))
 // nobitex.trades({ type: 'BTCIRT' }).then(res => console.log(res))
 
-// BTCIRT, ETHIRT, LTCIRT, XRPIRT, BCHIRT, BNBIRT, EOSIRT
-// XLMIRT, ETCIRT, TRXIRT, DOGEIRT, UNIIRT, DAIIRT, LINKIRT
-// DOTIRT, AAVEIRT, ADAIRT, SHIBIRT, USDTIRT, BTCUSDT, ETHUSDT
-// LTCUSDT, XRPUSDT, BCHUSDT, BNBUSDT, EOSUSDT, XLMUSDT, ETCUSDT 
-// TRXUSDT, PMNUSDT, DOGEUSDT, UNIUSDT, DAIUSDT, LINKUSDT, DOTUSDT
-// AAVEUSDT, ADAUSDT, SHIBUSDT
+/* Visit  https://github.com/sdsd From Seeing Objects*/
+
+
 
 
 // nobitex.market({from:'btc',to:'rls'}).then(res => console.log(res))
@@ -25,4 +22,4 @@ const { token } = require('./file.json')
 
 // nobitex.global().then(res => console.log(res))
 
-nobitex.account({ token: token }).then(res => console.log(res))
+// nobitex.account({ token: token }).then(res => console.log(res))
