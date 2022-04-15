de220399d3b4a0a95eb3623ff0083367e677e678
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-15 11:51:13 +0430

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const nobitex = require('./src/main') 
+const nobitex = require('./src/main')
 // const nobitex = require(nobitex.js)
 const { token } = require('./file.json')
 
@@ -7,5 +7,5 @@ const { token } = require('./file.json')
 // nobitex.Trades({ type: '' }).then(res => console.log(res))
 // nobitex.Market({from:'',to:''}).then(res => console.log(res))
 // nobitex.Global().then(res => console.log(res))
-// nobitex.Account({ token: token }).then(res => console.log(res))
-nobitex.CreateWallet
+// nobitex.Account({ token }).then(res => console.log(res))
+nobitex.CreateWallet({ token, type: '' }).then(res => console.log(res))
