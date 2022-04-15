ea9617ed1600da4f630f8d1bd5cf26df13ed4bd0
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-15 12:37:36 +0430

    Create AddBankAccount.module.js

---
+++ src/modules/AddBankAccount.module.js
@@ -0,0 +1,18 @@
+
+const axios = require('axios');
+module.exports = ({ token, CardNumber, Shaba ,BankName}) => {
+    const config = {
+        method: 'post',
+        url: 'https://api.nobitex.ir/users/accounts-add',
+        headers: {
+            Authorization: `Token ${token}`,
+        },
+        data: { "number": CardNumber, "shaba": Shaba , "bank": BankName}
+    };
+    return new Promise((resolve) => {
+        axios(config).then(data => {
+            const req = data.data;
+            resolve(req)
+        }).catch(e => { console.log(e) });
+    });
+}
