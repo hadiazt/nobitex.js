7fbdc6a961c79bf62be33567b7a113943df2c28a
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-15 15:22:05 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
+// Market Information (General)
 exports.Price = require('./modules/Price.module.js')
 exports.Trades = require('./modules/Trades.module.js')
 exports.Market = require('./modules/Market.module.js')
 exports.Global = require('./modules/Global.module.js')
+
+// Trading In The Market
+
+// User Information
 exports.Account = require('./modules/Account.module.js')
 exports.CreateWallet = require('./modules/CreateWallet.module.js')
 exports.AddCard = require('./modules/AddCard.module.js')
 exports.AddBankAccount = require('./modules/AddBankAccount.module.js')
 exports.AccountLimitations = require('./modules/AccountLimitations.module.js')
+
+// User Financial Information
 exports.AccountWallets = require('./modules/AccountWallets.module.js')
+
+// Security
