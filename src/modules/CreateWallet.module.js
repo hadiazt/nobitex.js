853b2c4421b1cf7acfb2ccfa76194cfc01de177f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-15 11:58:27 +0430

    Update CreateWallet.module.js

--- src/modules/CreateWallet.module.js
+++ src/modules/CreateWallet.module.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-module.exports = ({ token,type }) => {
+module.exports = ({ token, type }) => {
     const config = {
         method: 'post',
         url: 'https://api.nobitex.ir/users/wallets/generate-address',
@@ -10,16 +10,13 @@ module.exports = ({ token,type }) => {
     };
     return new Promise((resolve) => {
         axios(config).then(data => {
-            const req = data.data.profile;
+            const req = data.data;
             resolve({
-                Account: { AccountID: req.id, Username: req.username, Email: req.email, Level: req.level, },
-                Personal: { Firstname: req.firstName, Lastname: req.lastName, NationalCode: req.nationalCode, Nickname: req.nickname, Phone: req.phone, Mobile: req.mobile, Province: req.province, City: req.city, Address: req.address, },
-                BankDetails: { BankCards: req.bankCards, BankAccounts: req.bankAccounts },
-                Verifications: { Email: req.verifications.email, Phone: req.verifications.phone, Mobile: req.verifications.mobile, Identity: req.verifications.identity, Selfie: req.verifications.selfie, Auto_KYC: req.verifications.auto_kyc, BankAccount: req.verifications.bankAccount, BankCard: req.verifications.bankCard, Address: req.verifications.address, City: req.verifications.city, PhoneCode: req.verifications.phoneCode, MobileIdentity: req.verifications.mobileIdentity },
-                Options: { Fee: req.options.fee, FeeUSDT: req.options.feeUsdt, Discount: req.options.discount, IsManualFee: req.options.isManualFee, TFA: req.options.tfa, SocialLoginEnabled: req.options.socialLoginEnabled },
-                Features: req.features, ChatID: req.chatId, WithdrawEligible: req.withdrawEligible,
+                Status:req.status,
+                Address: req.address,
+                Tag: req.tag
             })
-        }).catch(e => { console.log(e?.response?.data?.detail) });
+        }).catch(e => { console.log(e) });
     });
 }
 
