71999b5b079f13e3e3554122eac4f4429be08923
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-18 19:34:37 +0430

    Update AddCard.module.js

--- src/modules/AddCard.module.js
+++ src/modules/AddCard.module.js
@@ -1,9 +1,9 @@
-
 const axios = require('axios');
+const { Base, Paths } = require('../config.json')
 module.exports = ({ token, CardNumber, BankName }) => {
     const config = {
         method: 'post',
-        url: 'https://api.nobitex.ir/users/cards-add',
+        url: Base + Paths.User + '/cards-add',
         headers: {
             Authorization: `Token ${token}`,
         },
