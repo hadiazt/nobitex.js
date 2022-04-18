e50bb9224b5682b2b65b6c509b04c57079048ad2
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-18 19:42:02 +0430

    Update Market.module.js

--- src/modules/Market.module.js
+++ src/modules/Market.module.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
+const { Base, Market } = require('../config.json')
 module.exports = ({ from, to }) => {
     const config = {
         method: 'post',
-        url: 'https://api.nobitex.ir/market/stats',
+        url: Base + Market,
         data: { "srcCurrency": from, "dstCurrency": to }
     };
     return new Promise((resolve) => {
         axios(config).then(data => {
-            const req = data.data.stats;        
+            const req = data.data.stats;
             resolve(JSON.stringify(req[from + '-' + to]))
         }).catch(e => { console.log(e?.response?.data?.detail) });
     });
