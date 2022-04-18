9753eac33c3388c9bd1c1ca383bcdb31e24b45be
hadi <65118892+hadiazt@users.noreply.github.com>
2022-04-18 19:48:04 +0430

    Update AccountBalance.module.js

--- src/modules/AccountBalance.module.js
+++ src/modules/AccountBalance.module.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-const { Base, Paths } = require('../config.json')
+const { Base, Wallet } = require('../config.json')
 module.exports = ({ token, type }) => {
     const config = {
         method: 'post',
-        url: Base + Paths.User + Paths.Wallet.Balance,
+        url: Base + Wallet.Balance,
         headers: {
             Authorization: `Token ${token}`,
         },
